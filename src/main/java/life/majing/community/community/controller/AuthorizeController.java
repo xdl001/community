b5387161412c02f3f034baa382bb6effcda667f7
xudeliang <xudeliang@unicloud.com>
2019-12-04 19:07:08 +0800

    exstract github config from code to properties

---
+++ src/main/java/life/majing/community/community/controller/AuthorizeController.java
@@ -0,0 +1,37 @@
+package life.majing.community.community.controller;
+
+import life.majing.community.community.dto.AccessTokenDTO;
+import life.majing.community.community.dto.GithubUser;
+import life.majing.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name="state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setCode(code);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
+        return "index";
+    }
+}
